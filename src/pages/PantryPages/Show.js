c5cb9b6c444bdbc1d2c65b58c4b96cc5e5c0a8fd
vernellmiller <nezeey@gmail.com>
2022-01-01 10:56:00 -0500

    adds form and helper functions for edit and delete

--- src/pages/PantryPages/Show.js
+++ src/pages/PantryPages/Show.js
@@ -1,11 +1,71 @@
-import React from "react"
+import { useState } from 'react';
 
-const Show = () => {
-    return (
-        <div>
-            <h1>Show Page</h1>
-        </div>
-    )
+const Show = (props) => {
+    const id = props.match.params.id;
+    const pantries = props.pantries;
+    const pantry = pantries.find((p) => p._id === id);
+
+    const[editForm, setEditForm] = useState(pantry);
+
+    const handleChange = (event) => {
+        setEditForm((prevState) => ({
+          ...prevState,
+          [event.target.name]: event.target.value,
+        }));
+      };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const{ _id, name, image, location, description} = editForm;
+        props.updatePantries({name, image, location, description}, _id);
+        props.history.push('/');
+    }; 
+
+    const removePantry = () => {
+        props.deletePantries(pantry._id);
+        props.history.push('/');
+    };
+
+        return (
+            <div className='pantry'>
+                <h1>{pantry.name}</h1>
+                <img src={pantry.image} alt={pantry.name} />
+                <h2>{pantry.location}</h2>
+                <h2>{pantry.description}</h2>
+                <button id="delete" onClick={removePantry}>DELETE</button>
+                <form onSubmit={handleSubmit}>
+                    <input 
+                    type="text"
+                    value={editForm.name}
+                    onChange={handleChange}
+                    placeholder="name"
+                    name="name"
+                    />
+                    <input 
+                    type="text"
+                    value={editForm.image}
+                    onChange={handleChange}
+                    placeholder="image URL"
+                    name="image"
+                    />
+                    <input 
+                    type="text"
+                    value={editForm.location}
+                    onChange={handleChange}
+                    placeholder="location"
+                    name="location"
+                    />
+                    <input 
+                    type="text"
+                    value={editForm.description}
+                    onChange={handleChange}
+                    placeholder="description"
+                    name="description"
+                    />
+                    <input type="submit" value="Edit Pantry" />
+                </form>
+            </div>
+        )
 }
 
 export default Show;
